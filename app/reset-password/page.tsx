cf3367d397356efe8c0300534c2d5d0e59c46ad3
Seneen Khan <Seneen.Khan@akaayis.com>
2026-01-29 17:43:13 +0530

    Added the Reset password page

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,10 @@
+import ResetPasswordForm from './ResetPasswordForm';
+
+export const metadata = {
+  title: 'Reset Password - SignPubliQ',
+  description: 'Reset your password',
+};
+
+export default function ResetPasswordPage() {
+  return <ResetPasswordForm />;
+}
